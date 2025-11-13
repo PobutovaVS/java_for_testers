ce4ca94e676b3fe2579384529fcdbc518f7e9ba7
PobutovaVS <krylovav4leria@yandex.ru>
2025-11-13 17:22:22 +0300

    Создание тестового класса, изменение тестов

--- adressbook_web_tests/src/test/java/GroupCreationTests.java
+++ adressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,17 +1,25 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void CanCreateGroup() {
         openGroupsPage();
-        createGroup("group_name", "group header", "group footer");
+        createGroup(new GroupData("name", "group header", "group footer"));
     }
 
     @Test
     public void CanCreateGroupWithEmptyName() {
         openGroupsPage();
-        createGroup("", "", "");
+        createGroup(new GroupData());
+    }
+
+    @Test
+    public void CanCreateGroupWithNameOnly() {
+        openGroupsPage();
+        var emptyGroup=new GroupData();
+        var groupWithName=emptyGroup.withName("some name");
+        createGroup(groupWithName);
     }
 }
