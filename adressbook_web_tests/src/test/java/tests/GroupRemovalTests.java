f5dc5f9a5e9dec171b05f44fa821b6711f6f11ac
PobutovaVS <krylovav4leria@yandex.ru>
2025-11-23 11:02:28 +0300

    Создание цикла перебор элементов коллекции

--- adressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ adressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -9,12 +9,21 @@ public class GroupRemovalTests extends TestBase {
     @Test
     public void CanRemoveGroup() {
         if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("", "", ""));
+            app.groups().createGroup(new GroupData("gg", "gg", "gg"));
         }
 
         int groupCount = app.groups().getCount();
         app.groups().removeGroup();
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount - 1, newGroupCount);
+    }
+
+    @Test
+    void canRemoveAllGroupsAtOnce() {
+        if (app.groups().getCount() == 0) {
+            app.groups().createGroup(new GroupData("gg", "gg", "gg"));
+        }
+        app.groups().removeAllGroups();
+        Assertions.assertEquals(0,app.groups().getCount());
     }
 }
