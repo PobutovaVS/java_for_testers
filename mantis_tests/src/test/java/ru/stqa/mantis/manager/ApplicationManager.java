bdff2daf78fe5ae9c23ccf5d0c9e5ea43060e841
PobutovaVS <krylovav4leria@yandex.ru>
2025-12-22 19:05:54 +0300

    Задание 17

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,51 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class ApplicationManager {
+
+    private WebDriver driver;
+    private String string;
+    private Properties properties;
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.string = browser;
+        this.properties = properties;
+
+    }
+
+    public WebDriver driver() {
+        if (driver == null) {
+            if ("firefox".equals(string)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(string)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknow browser %s", string));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit)); //когда джава будет завершаться? в самомо конце? надо вызвать функию
+            driver.get(properties.getProperty("web.baseUrl"));
+            driver.manage().window().setSize(new Dimension(1920, 1080));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        }
+        return driver;
+    }
+
+    public SessionHelper session(){
+        if (sessionHelper==null){
+            sessionHelper=new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+}
+
+
