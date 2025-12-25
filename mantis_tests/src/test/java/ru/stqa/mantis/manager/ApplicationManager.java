fcb41311a47aeacc081279b7d5db4b20ac2a14d3
PobutovaVS <krylovav4leria@yandex.ru>
2025-12-25 20:36:14 +0300

    Лекция  8.4, проблема

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -15,6 +15,8 @@ public class ApplicationManager {
     private String string;
     private Properties properties;
     private SessionHelper sessionHelper;
+    private HttpSessionHelper httpSessionHelper;
+    private JamesCliHelper jamesCliHelper;
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -40,11 +42,29 @@ public class ApplicationManager {
         return driver;
     }
 
-    public SessionHelper session(){
-        if (sessionHelper==null){
-            sessionHelper=new SessionHelper(this);
+    public SessionHelper session() {
+        if (sessionHelper == null) {
+            sessionHelper = new SessionHelper(this);
         }
         return sessionHelper;
+    }
+
+    public HttpSessionHelper http() {
+        if (httpSessionHelper == null) {
+            httpSessionHelper = new HttpSessionHelper(this);
+        }
+        return httpSessionHelper;
+    }
+
+    public JamesCliHelper jamesCli() {
+        if (jamesCliHelper == null) {
+            jamesCliHelper = new JamesCliHelper(this);
+        }
+        return jamesCliHelper;
+    }
+
+    public String property(String name) {
+        return properties.getProperty(name);
     }
 }
 
---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/JamesCliHelper.java
@@ -0,0 +1,24 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.os.ExternalProcess;
+
+
+import java.time.Duration;
+
+public class JamesCliHelper extends HelperBase {
+    public JamesCliHelper(ApplicationManager manager) {
+        super(manager);
+    }
+
+    public void addUser(String email, String password) throws InterruptedException {
+        ExternalProcess.builder()
+                .command("java", "-cp", "\"/Users/valeriyapobutova/Downloads/james-server-jpa-guice/james-server-jpa-app.lib/*\"",
+                        "org.apache.james.cli.ServerCmd",
+                        "AddUser", email, password)
+                .directory(manager.property("james.workingDir"))
+                .copyOutputTo(System.err)
+                .start()
+                .waitFor(Duration.ofHours(1));
+
+    }
+}
