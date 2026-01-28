d793e05cdc05dbd7eaecb05c4e12746925e867f0
Lera <krylovav4leria@yandex.ru>
2026-01-28 20:20:54 +0300

    Лекция 9.3

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -19,6 +19,7 @@ public class ApplicationManager {
     private JamesCliHelper jamesCliHelper;
     private MailHelper mailHelper;
     private JamesApiHelper jamesApiHelper;
+    private RestApiHelper restApiHelper;
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -78,6 +79,14 @@ public class ApplicationManager {
         return jamesApiHelper;
     }
 
+
+
+    public RestApiHelper rest() {
+        if (restApiHelper == null) {
+            restApiHelper = new RestApiHelper(this);
+        }
+        return restApiHelper;
+    }
     public String property(String name) {
         return properties.getProperty(name);
     }
